d69f74a95b41d01eda59ec455cc2caf05151e25c
zzw <kenzzw@163.com>
2019-07-15 22:14:04 +0800

    Refactor: refactor SuperSmartParkingBoyTest

--- src/test/java/com/thoughtworks/tdd/SuperSmartParkingBoyTest.java
+++ src/test/java/com/thoughtworks/tdd/SuperSmartParkingBoyTest.java
@@ -10,24 +10,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SuperSmartParkingBoyTest {
     @Test
-    public void mutiple_parkingt_lots_can_fetch_correct_car() throws Exception {
+    public void should_park_car_in_bigger_positions_rate_lot() throws Exception {
         List<ParkingLot> lots=new ArrayList<>();
         ParkingLot parkingLot1=new ParkingLot();
         ParkingLot parkingLot2=new ParkingLot();
         lots.add(parkingLot1);
         lots.add(parkingLot2);
         SuperSmartParkingBoy superSmartParkingBoy=new SuperSmartParkingBoy(lots);
-        Car firstCar=new Car();
-        Ticket ticketFirst=superSmartParkingBoy.parking(firstCar);
-        for(int i=0;i<19;i++){
+        for(int i=0;i<6;i++){
             Car car=new Car();
             Ticket ticket=superSmartParkingBoy.parking(car);
         }
-        Car firstCarFetch=superSmartParkingBoy.fetchCar(ticketFirst);
+        Car firstCar=new Car();
+        Ticket ticketFirst=superSmartParkingBoy.parking(firstCar);
 
-        Car car11=new Car();
-        Ticket ticketLast=superSmartParkingBoy.parking(car11);
-        Car fetchCarLast=superSmartParkingBoy.fetchCar(ticketLast);
-        Assertions.assertFalse(parkingLot1.getParkingMap().get(fetchCarLast)!=null);
+        Car SecondeCar=new Car();
+        Ticket ticketLast=superSmartParkingBoy.parking(SecondeCar);
+        Assertions.assertTrue(parkingLot2.getParkingMap().get(ticketLast)==null);
     }
+
 }
